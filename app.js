a5fb0da54f71d880b850261269161eba7bdf4d67
sivaraman251600 <sivaraman251600>
2022-03-28 10:35:46 +0530

    Complete the JavaScript for Color Change

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 function change(){
     document.getElementById("para").style.color = "green";
+    
+    var textContent = document.getElementById("light-dark").innerHTML;
 
-    if(document.getElementById("para").style.color.value == "green"){
-        
+    if(textContent == "Dark"){
+        document.body.style.backgroundColor = "black";
+        document.getElementById("para").style.color = "Green"
+        document.getElementById("light-dark").innerHTML = "Light";
+    }
+
+    if(textContent == "Light"){
+        document.body.style.backgroundColor = "White";
+        document.getElementById("para").style.color = "black";
+        document.getElementById("light-dark").innerHTML = "Dark";
     }
 }
